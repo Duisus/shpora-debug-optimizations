ae33b3e701a6a8105e6e4e4c271aea3d5e1b69dc
Denis Bautin <denisbduis@gmail.com>
2021-04-07 18:15:55 +0500

    Change Pixel

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -14,38 +14,38 @@ namespace JPEG.Images
                 throw new FormatException("Unknown pixel format: " + pixelFormat);
             Format = pixelFormat;
             
-            this.FirstComponent = firstComponent;
-            this.SecondComponent = secondComponent;
-            this.ThirdComponent = thirdComponent;
+            _firstComponent = firstComponent;
+            _secondComponent = secondComponent;
+            _thirdComponent = thirdComponent;
         }
 
-        public readonly byte FirstComponent;
-        public readonly byte SecondComponent;
-        public readonly byte ThirdComponent;
+        private readonly byte _firstComponent;
+        private readonly byte _secondComponent;
+        private readonly byte _thirdComponent;
 
         public double R =>
             Format == PixelFormat.RGB 
-                ? FirstComponent
-                : (298.082 * FirstComponent + 408.583 * ThirdComponent) / 256.0 - 222.921;
+                ? _firstComponent
+                : (298.082 * _firstComponent + 408.583 * _thirdComponent) / 256.0 - 222.921;
         public double G =>
             Format == PixelFormat.RGB 
-                ? SecondComponent 
-                : (298.082 * FirstComponent - 100.291 * SecondComponent - 208.120 * ThirdComponent) / 256.0 + 135.576;
+                ? _secondComponent 
+                : (298.082 * _firstComponent - 100.291 * _secondComponent - 208.120 * _thirdComponent) / 256.0 + 135.576;
         public double B => 
             Format == PixelFormat.RGB 
-                ? ThirdComponent 
-                : (298.082 * FirstComponent + 516.412 * SecondComponent) / 256.0 - 276.836;
+                ? _thirdComponent 
+                : (298.082 * _firstComponent + 516.412 * _secondComponent) / 256.0 - 276.836;
         public double Y => 
             Format == PixelFormat.YCbCr 
-                ? FirstComponent 
-                : 16.0 + (65.738 * FirstComponent + 129.057 * SecondComponent + 24.064 * ThirdComponent) / 256.0;
+                ? _firstComponent 
+                : 16.0 + (65.738 * _firstComponent + 129.057 * _secondComponent + 24.064 * _thirdComponent) / 256.0;
         public double Cb => 
             Format == PixelFormat.YCbCr 
-                ? SecondComponent 
-                : 128.0 + (-37.945 * FirstComponent - 74.494 * SecondComponent + 112.439 * ThirdComponent) / 256.0;
+                ? _secondComponent 
+                : 128.0 + (-37.945 * _firstComponent - 74.494 * _secondComponent + 112.439 * _thirdComponent) / 256.0;
         public double Cr => 
             Format == PixelFormat.YCbCr 
-                ? ThirdComponent 
-                : 128.0 + (112.439 * FirstComponent - 94.154 * SecondComponent - 18.285 * ThirdComponent) / 256.0;
+                ? _thirdComponent 
+                : 128.0 + (112.439 * _firstComponent - 94.154 * _secondComponent - 18.285 * _thirdComponent) / 256.0;
     }
 }
